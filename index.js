ce32a8503526f681c73edff9c3978edfda6b8fbc
DefectiveFox <niklas.spz@gmail.com>
2021-08-20 16:52:39 +0200

    no

--- index.js
+++ index.js
@@ -11,15 +11,39 @@ data = data.map((song) => {
 		  seconds = Math.floor((duration / 1000) % 60),
 		  minutes = Math.floor((duration / (1000 * 60)) % 60),
 		  hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-	  
+
 		hours = (hours == 0) ? "" : hours + ":";
 		minutes = (minutes < 10) ? "0" + minutes : minutes;
 		seconds = (seconds < 10) ? "0" + seconds : seconds;
-		
+
 		return hours + minutes + ":" + seconds;
 	}
 	song.displayLength = msToTime(song.songlength);
-	
+
+	function shitToColor(match, p, color, text) {
+		return `<span style="color: ${color}">${text}</span>`;
+	}
+
+	song.Name = song.Name.replace(/<\s*color\s*=(")?([^"]+)\1>(.+?)<\s*\/color\s*>/g, shitToColor);
+	song.Artist = song.Artist.replace(/<\s*color\s*=(")?([^"]+)\1>(.+?)<\s*\/color\s*>/g, shitToColor);
+	song.Album = song.Album.replace(/<\s*color\s*=(")?([^"]+)\1>(.+?)<\s*\/color\s*>/g, shitToColor);
+	song.Genre= song.Genre.replace(/<\s*color\s*=(")?([^"]+)\1>(.+?)<\s*\/color\s*>/g, shitToColor);
+	song.Charter = song.Charter.replace(/<\s*color\s*=(")?([^"]+)\1>(.+?)<\s*\/color\s*>/g, shitToColor);
+	song.Year = song.Year.replace(/<\s*color\s*=(")?([^"]+)\1>(.+?)<\s*\/color\s*>/g, shitToColor);
+	song.Playlist = song.Playlist.replace(/<\s*color\s*=(")?([^"]+)\1>(.+?)<\s*\/color\s*>/g, shitToColor);
+
+/*
+"Name": "Yu-Gi-Oh! Theme Song",
+"Artist": "TwitchPresents",
+"Album": "Twitch",
+"Genre": "Memes",
+"Charter": "Helvian",
+"Year": "1776",
+"Playlist": "helvianalects",
+*/
+
+
+
 	return song;
 })
 
